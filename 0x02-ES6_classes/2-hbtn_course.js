c9cf62c13bebf1858f70d36ebc457d3ec9551060
qoudri4re <qoudri4re@gmail.com>
2022-12-22 20:56:37 +0100

    fixed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = length;
+    this.name = name;
+    this.length = length;
+    this.students = length;
   }
 
   get name() {
