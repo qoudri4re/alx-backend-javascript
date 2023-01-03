7347df9a1f390800683300adba27461f589a579e
qoudri4re <qoudri4re@gmail.com>
2023-01-04 00:11:23 +0100

    task 10

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-ii
+export default function groceriesList() {
+  const map = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return map;
+}
