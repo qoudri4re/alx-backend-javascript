a55b178590936eb30a4bfae6a9de599dc7fe45b8
qoudri4re <qoudri4re@gmail.com>
2023-01-04 00:05:01 +0100

    task 9

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,16 @@
 export default function cleanSet(set, startString) {
-  let newString = [];
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      newString.push(value.substring(startString.length));
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
-  });
-  return newString.join('-');
+  }
+  return parts.join('-');
 }
