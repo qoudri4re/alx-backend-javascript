4bbe6ece29c0f839fe23a14b756f2d52e2103493
qoudri4re <qoudri4re@gmail.com>
2023-02-24 16:56:44 +0100

    --

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -7,10 +7,40 @@ describe("calculateNumber", function () {
     it("correct result for addition", () => {
       expect(calculateNumber("SUM", 1.0, 2.0)).to.equal(3);
     });
+    it("equal positive numbers (alternate)", () => {
+      expect(calculateNumber("SUM", 2.3, 1.8)).to.equal(4);
+    });
+
+    it("equal negative numbers", () => {
+      expect(calculateNumber("SUM", -2.0, -2.0)).to.equal(-4);
+    });
+
+    it("equal negative numbers (alternate)", () => {
+      expect(calculateNumber("SUM", -2.3, -1.8)).to.equal(-4);
+    });
+
+    it("negative and positive numbers", () => {
+      expect(calculateNumber("SUM", -2.0, 2.0)).to.equal(0);
+    });
   });
   describe('type == "SUBTRACT"', () => {
     it("correct result for subtraction", () => {
       expect(calculateNumber("SUBTRACT", 1.0, 2.4)).to.equal(-1);
+    });
+    it("equal positive numbers (alternate)", () => {
+      expect(calculateNumber("SUBTRACT", 2.3, 1.8)).to.equal(0);
+    });
+
+    it("equal negative numbers", () => {
+      expect(calculateNumber("SUBTRACT", -2.0, -2.0)).to.equal(0);
+    });
+
+    it("equal negative numbers (alternate)", () => {
+      expect(calculateNumber("SUBTRACT", -2.3, -1.8)).to.equal(0);
+    });
+
+    it("negative and positive numbers", () => {
+      expect(calculateNumber("SUBTRACT", -2.0, 2.0)).to.equal(-4.0);
     });
   });
   describe('type == "DIVISION"', () => {
@@ -18,7 +48,18 @@ describe("calculateNumber", function () {
       expect(calculateNumber("DIVISION", 1.4, 2.4)).to.equal(0.5);
     });
     it("return error on zero division", () => {
-      expect(calculateNumber(1.4, 0.0)).to.equal("Error");
+      expect(calculateNumber("DIVISION", 1.4, 0.0)).to.equal("Error");
+    });
+    it("numbers with different signs (alternate)", () => {
+      expect(calculateNumber("DIVIDE", 7.0, -2.0)).to.equal(-3.5);
+    });
+
+    it("negative numbers", () => {
+      expect(calculateNumber("DIVIDE", -7.0, -2.0)).to.equal(3.5);
+    });
+
+    it("equal positive numbers", () => {
+      expect(calculateNumber("DIVIDE", 2.0, 2.0)).to.equal(1);
     });
   });
 });
