049df20d24df0832fb04a7c14740472fcd5164b0
qoudri4re <qoudri4re@gmail.com>
2023-02-24 11:15:16 +0100

    --

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,1 +1,13 @@
-iii
+function calculateNumber(type, a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+
+  if (type === "SUM") {
+    return a + b;
+  } else if (type === "SUBTRACT") {
+    return a - b;
+  } else {
+    return b > 0 ? a / b : "Error";
+  }
+}
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,24 @@
-ii
+const assert = require("assert");
+const calculateNumber = require("./1-calcul");
+const { expect } = require("chai");
+
+describe("calculateNumber", function () {
+  describe('type == "SUM"', () => {
+    it("correct result for addition", () => {
+      expect(calculateNumber("SUM", 1.0, 2.0)).to.equal(3);
+    });
+  });
+  describe('type == "SUBTRACT"', () => {
+    it("correct result for subtraction", () => {
+      expect(calculateNumber("SUBTRACT", 1.0, 2.4)).to.equal(-1);
+    });
+  });
+  describe('type == "DIVISION"', () => {
+    it("correct result for division", () => {
+      expect(calculateNumber("DIVISION", 1.4, 2.4)).to.equal(0.5);
+    });
+    it("return error on zero division", () => {
+      expect(calculateNumber(1.4, 0.0)).to.equal("Error");
+    });
+  });
+});
